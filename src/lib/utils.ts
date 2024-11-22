48d1c2c03ebf1fc9e064aec2ac1348e6508dfc2e
Tim Everett <tim.neil.everett@gmail.com>
2024-11-22 06:08:21 -0400

    fix: shadcn installation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,40 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-export const dateToDateString = (date: Date) => {
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
-  const month = monthNames[date.getMonth()];
-  const day = date.getDate();
-  const year = date.getFullYear();
-
-  return `${month} ${day} ${year}`;
-};
-
-export const getSiteURL = () => {
-  let url = import.meta.env.VITE_SITE_URL ?? // Set this to your site URL in production env.
-    import.meta.env.VITE_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:8100/";
-  // Make sure to include `https://` when not localhost.
-  url = url.startsWith("http") ? url : `https://${url}`;
-  // Make sure to include a trailing `/`.
-  url = url.endsWith("/") ? url : `${url}/`;
-  return url;
-};
